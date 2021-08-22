f5908f3c736d9d2f6b1480d52a8737f2e62d929d
Loknath <lokabanerjee@gmail.com>
2021-08-22 23:15:53 +0530

    add sidebar in user dashboard

--- smartcontactmanager/src/main/resources/static/js/script.js
+++ smartcontactmanager/src/main/resources/static/js/script.js
@@ -1,1 +1,16 @@
 console.log("This is script file")
+
+
+const toggleSidebar = ()=>{
+
+    if($(".sidebar").is(":visible")){
+        
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+  //      $('.content').css("padding-left", "18%");
+    }else{
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+//		$('.content').css("padding-left", "1%");
+    }
+};
